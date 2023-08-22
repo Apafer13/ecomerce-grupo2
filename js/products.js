bbd92586094be80f5e0a5c0ccc1b519b6dcd29ff
Apafer13 <apafer11@gmail.com>
2023-08-22 16:04:42 -0300

    punto 2

--- js/products.js
+++ js/products.js
@@ -1,9 +1,42 @@
-const URL_Autos = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+const URLS = ["https://japceibal.github.io/emercado-api/cats_products/101.json", 
+"https://japceibal.github.io/emercado-api/cats_products/102.json",
+"https://japceibal.github.io/emercado-api/cats_products/103.json",
+"https://japceibal.github.io/emercado-api/cats_products/104.json",
+"https://japceibal.github.io/emercado-api/cats_products/105.json",
+"https://japceibal.github.io/emercado-api/cats_products/106.json",
+"https://japceibal.github.io/emercado-api/cats_products/107.json",
+"https://japceibal.github.io/emercado-api/cats_products/108.json",
+"https://japceibal.github.io/emercado-api/cats_products/109.json"];
 
-function showData(Array) {
+function showData(dataArray) {
     let auto = document.getElementById("cat-list-container");
     let htmlContentToAppend = "";
-    for (const item of Array) {
+    let idCat = localStorage.getItem("catID")
+    for(i=0; i<dataArray.length;i++){    
+        if (idCat == dataArray[i].catID){
+            for (const item of dataArray[i].products) {
+                htmlContentToAppend += `
+                
+                <div class="list-group-item list-group-item-action cursor-active">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="${item.image}" class="img-thumbnail">
+                        </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">${item.name}</h4>
+                            <small class="text-muted">${item.soldCount} vendidos</small>
+                        </div>
+                        <p class="mb-1">${item.description}</p>
+                        </div>
+                    </div>
+                </div>`
+            auto.innerHTML = htmlContentToAppend;
+            
+        }
+    }}
+    if (idCat == data.catID){
+    for (const item of dataArray) {
         htmlContentToAppend += `
         <div class="list-group-item list-group-item-action cursor-active">
             <div class="row">
@@ -22,7 +55,10 @@ function showData(Array) {
         auto.innerHTML = htmlContentToAppend;
     }
 }
-
-fetch(URL_Autos)
-    .then(response => response.json())
-    .then(data => showData(data.products))
+}
+const fetchPromises = URLS.map(url => fetch(url));
+Promise.all(fetchPromises)
+  .then(responses => Promise.all(responses.map(response => response.json())))
+  .then(dataArray => {
+    showData(dataArray)
+  })
