089e4f84961db599a654d03ed86232f496da76fb
Apafer13 <apafer11@gmail.com>
2023-08-22 16:29:06 -0300

    Terminado

--- js/products.js
+++ js/products.js
@@ -16,7 +16,6 @@ function showData(dataArray) {
         if (idCat == dataArray[i].catID){
             for (const item of dataArray[i].products) {
                 htmlContentToAppend += `
-                
                 <div class="list-group-item list-group-item-action cursor-active">
                     <div class="row">
                         <div class="col-3">
@@ -35,30 +34,30 @@ function showData(dataArray) {
             
         }
     }}
-    if (idCat == data.catID){
-    for (const item of dataArray) {
+}
+function titulo(dataArray) {
+    let tituloCat = document.getElementById("titulo");
+    let htmlContentToAppend = "";
+    let idCat = localStorage.getItem("catID");
+  
+    for (let i = 0; i < dataArray.length; i++) {
+      if (idCat == dataArray[i].catID) {
         htmlContentToAppend += `
-        <div class="list-group-item list-group-item-action cursor-active">
-            <div class="row">
-                <div class="col-3">
-                    <img src="${item.image}" class="img-thumbnail">
-                </div>
-            <div class="col">
-                <div class="d-flex w-100 justify-content-between">
-                    <h4 class="mb-1">${item.name}</h4>
-                    <small class="text-muted">${item.soldCount} vendidos</small>
-                </div>
-                <p class="mb-1">${item.description}</p>
-                </div>
-            </div>
-         </div>`
-        auto.innerHTML = htmlContentToAppend;
+          <h1>Productos</h1>
+          <p>Verás aquí todos los productos de la categoría ${dataArray[i].catName}</p>
+        `;
+      }
     }
-}
-}
+  
+    tituloCat.innerHTML = htmlContentToAppend;
+  }
+  window.addEventListener('load', function() {
+    titulo();
+ });
 const fetchPromises = URLS.map(url => fetch(url));
 Promise.all(fetchPromises)
   .then(responses => Promise.all(responses.map(response => response.json())))
   .then(dataArray => {
-    showData(dataArray)
+    titulo(dataArray);
+    showData(dataArray);
   })
