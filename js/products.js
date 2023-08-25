2a9462ad9cd9784f810126bb34258d41a17a782e
Kianny1999 <141190675+Kianny1999@users.noreply.github.com>
2023-08-25 12:55:14 -0300

    filtro en tiempo real

--- js/products.js
+++ js/products.js
@@ -1,14 +1,56 @@
 const URL_Autos = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+const buscador = document.getElementById("filtrado")
+let productos = [];
+let container = document.getElementById("cat-list-container");
 
-function showData(data) {
+function dataConverter(data) {
+    return data.products;
+}
+
+function addItem(container, item) {
+    container.innerHTML += `<div class="list-group-item list-group-item-action cursor-active">
+    <div class="row">
+        <div class="col-3">
+            <img src="${item.image}" class="img-thumbnail">
+        </div>
+    <div class="col">
+        <div class="d-flex w-100 justify-content-between">
+            <h4 class="mb-1">${item.name}</h4>
+            <small class="text-muted">${item.soldCount} vendidos</small>
+        </div>
+        <p class="mb-1">${item.description}</p>
+        </div>
+    </div>
+ </div>`
+}
+
+function showData(filteredProducts) {
+    container.innerHTML = ''; // Limpiar el contenedor antes de agregar los nuevos resultados
+    for (const item of filteredProducts) {
+        addItem(container, item);
+    }}
+
+    fetch(URL_Autos)
+    .then(response => response.json())
+    .then(data => {
+        productos = dataConverter(data); // Aquí asignas los productos obtenidos del JSON
+        showData(productos); // Mostrar todos los productos al principio
+    });
+
+buscador.addEventListener('input', function() {
+    showData(productos.filter(producto => producto.name.toLowerCase().includes(buscador.value.toLowerCase())));
+})
+
+
+/*     29 30 if(data.catID == idCategoria) {
+        productos = data.products; */
+
+/* function showData(data) {
     let auto = document.getElementById("cat-list-container");
     let htmlContentToAppend = "";
-    let Array=data.products;
     let idCategoria=localStorage.getItem("catID")
 
-    if(data.catID==idCategoria) {
-      
-    for (const item of Array) {
+   
 
         htmlContentToAppend += `
         <div class="list-group-item list-group-item-action cursor-active">
@@ -28,8 +70,4 @@ function showData(data) {
         auto.innerHTML = htmlContentToAppend;
     }
 }
-}
-fetch(URL_Autos)
-    .then(response => response.json())
-    .then(data => showData(data))
-    
+} */
