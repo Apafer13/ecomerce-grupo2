f34a66f1325c4d25b5acf5b25def20b52140ce05
Apafer13 <apafer11@gmail.com>
2023-09-14 22:35:28 -0300

    lol

--- js/product-info.js
+++ js/product-info.js
@@ -1,5 +1,7 @@
 let ProdID = localStorage.getItem("ProdID");
+let divProducto = document.getElementById("infoProducto");
 PRODUCTS_COMMENTS = `https://japceibal.github.io/emercado-api/products_comments/${ProdID}.json`;
+let URL_PRODUCTO = `https://japceibal.github.io/emercado-api/products/${ProdID}.json`;
 let filledStar = `<i class="fas fa-star" style="color: #fff947;"></i>`
 let emptyStar = `<i class="far fa-star" style="color: #000000;"></i>`
 function hora(){
@@ -60,9 +62,46 @@ btnSend.addEventListener("click", function(){
 })
 
 
+var options = {  year: 'numeric', month: 'numeric', day: 'numeric' };
+options.timeZone = 'GMT-3';
 
 
 document.addEventListener("DOMContentLoaded", function () {
+    fetch(URL_PRODUCTO)
+    .then(response => response.json())
+    .then(data => {
+        let htmlContentToAppend = "";
+
+        htmlContentToAppend += `
+                    <h2>${data.name}</h2>
+                    <hr>
+                    <h4>Precio</h4>
+                    <p>${data.currency} ${data.cost}</p>
+                    <br>
+                    <h4>Descripción</h4>
+                    <p>${data.description}</p>
+                    <br>
+                    <h4>Categoría</h4>
+                    <p>${data.category}</p>
+                    <br>
+                    <h4>Cantidad de vendidos</h4>
+                    <p>${data.soldCount}</p>
+                    <br>
+                    <h4>Imágenes ilustrativas</h4>
+                    <div class="estiloimg">
+                    `
+                  for (let i of data.images) {
+                    htmlContentToAppend += 
+                    `<img src="${i}" class="img-thumbnail">`
+                   }
+                   htmlContentToAppend += `</div>`
+
+        divProducto.innerHTML = htmlContentToAppend;
+
+    })
+    .catch(error => {
+        console.error("Error al cargar los datos:", error);
+    });
     getJSONData(PRODUCTS_COMMENTS)
         .then(result => {
             if (result.status === "ok") {
