1c66a307daa9904daf0cf770176d445acd102cfc
Emilia <141192796+emseis@users.noreply.github.com>
2023-10-06 18:56:33 -0300

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,45 +1,80 @@
 document.addEventListener("DOMContentLoaded", ()=>{
-const url = "https://japceibal.github.io/emercado-api/user_cart/25801.json"
-getJSONData(url)
-.then(result => {
-    if (result.status === "ok") {
-        ShowCartData(result.data.articles[0]);
+    const url = "https://japceibal.github.io/emercado-api/user_cart/25801.json"
+    getJSONData(url)
+    .then(result => {
+        if (result.status === "ok") {
+            ShowCartData(result.data.articles[0]);
+            
+        } else {
+            console.error(result.data);
+        }
+    });
+    
+    const shippingTypeSelect = document.getElementById("shippingType");
+    const streetInput = document.getElementById("street");
+    const numberInput = document.getElementById("number");
+    const cornerInput = document.getElementById("corner");
+    const confirmButton = document.getElementById("confirmButton");
+    
+    confirmButton.addEventListener("click", confirmShipping);
+    
+    function confirmShipping() {
+        const selectedShippingType = shippingTypeSelect.value;
+        const selectedStreet = streetInput.value;
+        const selectedNumber = numberInput.value;
+        const selectedCorner = cornerInput.value;
+    
         
-    } else {
-        console.error(result.data);
     }
-});
-
-  
-
-
-})
-
-
-
-function ShowCartData(data){
-    let cart = document.getElementById('infoCart')
-    let htmlContentToAppend = ""
-    htmlContentToAppend+= `
-        <table class="table" id="tableCart">
-            <thead>
-                <tr>
-                    <th scope="col" class="col-sm-1"></th>
-                    <th scope="col" class="col-sm-1">Nombre</th>
-                    <th scope="col" class="col-sm-1">Costo</th>
-                    <th scope="col" class="col-sm-1">Cantidad</th>
-                    <th scope="col" class="col-sm-1">Subtotal</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td><img class="img-fluid" src="${data.image}" alt=""></td>
-                    <td class="align-middle">${data.name}</td>
-                    <td class="align-middle">${data.currency} ${data.unitCost}</td>
-                    <td class="align-middle"><input type="number" value="${data.count}" class="w-25 text-center"></td>
-                    <td class="align-middle"><b>${data.unitCost}</b></td>
-                </tr>
-            </tbody>
-        </table>`
-        cart.innerHTML = htmlContentToAppend
-}
+    
+    })
+    
+    
+    function ShowCartData(data){
+        let cart = document.getElementById('infoCart')
+        let htmlContentToAppend = ""
+        htmlContentToAppend+= `
+            <table class="table" id="tableCart">
+                <thead>
+                    <tr>
+                        <th scope="col" class="col-sm-1"></th>
+                        <th scope="col" class="col-sm-1">Nombre</th>
+                        <th scope="col" class="col-sm-1">Costo</th>
+                        <th scope="col" class="col-sm-1">Cantidad</th>
+                        <th scope="col" class="col-sm-1">Subtotal</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><img class="img-fluid" src="${data.image}" alt=""></td>
+                        <td class="align-middle">${data.name}</td>
+                        <td class="align-middle">${data.currency} ${data.unitCost}</td>
+                        <td class="align-middle"><input type="number" value="${data.count}" class="w-25 text-center"></td>
+                        <td class="align-middle"><b>${data.unitCost}</b></td>
+                    </tr>
+                </tbody>
+            </table>`;
+            cart.innerHTML = htmlContentToAppend
+    
+            let shippingHtml = `
+            <label for="shippingType">Tipo de Envío:</label>
+            <select id="shippingType" name="shippingType">
+              <option value="premium">Premium 2 a 5 días (15%)</option>
+              <option value="express">Express 5 a 8 días (7%)</option>
+              <option value="standard">Standard 12 a 15 días (5%)</option>
+            </select>
+         
+            <label for="street">Calle:</label>
+            <input type="text" id="street" name="street" required>
+         
+            <label for="number">Número:</label>
+            <input type="text" id="number" name="number" required>
+         
+            <label for="corner">Esquina:</label>
+            <input type="text" id="corner" name="corner">
+         
+            <button id="confirmButton">Confirmar Envío</button>`;
+    
+            cart.innerHTML += shippingHtml;
+    
+    }
