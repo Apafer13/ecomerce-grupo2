421d2e912a68340c97dca731c8e6f7a25f4cd750
Aparicio Fernandez <89668234+Apafer13@users.noreply.github.com>
2023-10-12 18:42:26 -0300

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,68 @@
+let cart = document.getElementById('infoCart');
+
+document.addEventListener("DOMContentLoaded", () => {
+    const url = "https://japceibal.github.io/emercado-api/user_cart/25801.json";
+    getJSONData(url)
+        .then(result => {
+            if (result.status === "ok") {
+                ShowCartData(result.data.articles[0]);
+                completarCarro();
+            } else {
+                console.error(result.data);
+            }
+        });
+});
+
+function modificar(data) {
+    let subtotalProducto = document.getElementById('subtotalProducto');
+    let cant = document.getElementById('cantidad');
+    let cantidad = parseInt(cant.value);
+    let subtotal = parseInt(cantidad * data);
+    subtotalProducto.innerHTML = `${subtotal}`;
+}
+
+function recalcular() {
+    let cantidad = document.getElementsByClassName("cantidad");
+    let costo = document.getElementsByClassName("costo");
+    let subTotal = document.getElementsByClassName("subtotal");
+
+    for (let i = 0; i < cantidad.length; i++) {
+        let cantidadValor = parseInt(cantidad[i].value);
+        let costoValor = parseFloat(costo[i].textContent);
+        let subtotal = cantidadValor * costoValor;
+        subTotal[i].textContent = subtotal;
+    }
+}
+
+
+function ShowCartData(data) {
+    let htmlContentToAppend = "";
+    htmlContentToAppend += `<tr>   
+                    <td><img class="img-fluid" src="${data.image}" alt=""></td>
+                    <td class="align-middle">${data.name}</td>
+                    <td class="align-middle">${data.currency} ${data.unitCost}</td>
+                    <td class="align-middle"><input type="number" value="${data.count}" class="w-25 text-center" min="1" id="cantidad" onchange="modificar(${data.unitCost})"></td>
+                    <td class="align-middle"><b>${data.currency}&nbsp;</b><b id="subtotalProducto">${data.unitCost}</b></td>
+                    </tr>`;
+    cart.innerHTML = htmlContentToAppend;
+}
+
+function completarCarro() {
+    let carrito = JSON.parse(localStorage.getItem("Carrito"));
+    let htmlContentToAppend = "";
+
+    for (let producto of carrito) {
+        htmlContentToAppend += `<tr>   
+        <td><img class="img-fluid" src="${producto.image}" alt=""></td>
+        <td class="align-middle">${producto.name}</td>
+        <td class="align-middle">${producto.currency} <span class="costo">${producto.unitCost}</span></td>
+        <td class="align-middle"><input type="number" value="${producto.count}" class="w-25 text-center cantidad" min="1"  onchange="recalcular()"></td>
+        <td class="align-middle"><b>${producto.currency}&nbsp;</b><b> <span class="subtotal">${producto.unitCost}</span></b></td>
+        </tr>`;
+    }
+    cart.innerHTML += htmlContentToAppend;
+}
+=======
 document.addEventListener("DOMContentLoaded", ()=>{
 const url = "https://japceibal.github.io/emercado-api/user_cart/25801.json"
 getJSONData(url)
