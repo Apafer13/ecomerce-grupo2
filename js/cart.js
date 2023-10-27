cff15a19fe122d930ac90638007c6ca2cce905af
unknown <pauarcieri@gmail.com>
2023-10-26 22:19:58 -0300

    pauta 3 - versión 2

---
+++ js/cart.js
@@ -0,0 +1,218 @@
+let cart = document.getElementById('infoCart');
+
+document.addEventListener("DOMContentLoaded", () => {
+    const url = "https://japceibal.github.io/emercado-api/user_cart/25801.json";
+    getJSONData(url)
+        .then(result => {
+            if (result.status === "ok") {
+                ShowCartData(result.data.articles[0]);
+                completarCarro();
+            } else {
+                console.error(result.data);
+            }
+        });
+
+    const paymentTypeDisplay = document.getElementById("payment-type-display");    
+    const selectPaymentButton = document.getElementById("select-payment-button");
+    const paymentSelection = document.getElementById("payment-selection");
+    const paymentModal = document.getElementById("payment-modal");
+    const closeModal = document.getElementById("close-modal");
+    const savePayment = document.getElementById("save-payment");
+    const paymentOptions = document.getElementsByName("payment-option");
+    const creditCardFields = document.getElementById("credit-card-fields");
+    const bankTransferFields = document.getElementById("bank-transfer-fields");
+    let formModal = document.getElementById("formModal");
+    
+
+
+//Muestra la ventana Modal cuando se hace click en "seleccionar"
+    selectPaymentButton.addEventListener("click", () => {
+        paymentModal.style.display = "block";
+    });
+
+    // Para cerrar el modal haciendo click en "X"
+    closeModal.addEventListener("click", () => {
+        paymentModal.style.display = "none";
+    });
+
+// Guardar la forma de pago
+    savePayment.addEventListener("click", () => {
+        let selectedPayment = "";
+        
+        paymentOptions.forEach((option) => {
+            if (option.checked) {
+                selectedPayment = option.value;
+                paymentSelection.innerHTML = selectedPayment;
+                
+            } 
+        });
+        if (formModal.checkValidity()) {
+        paymentModal.style.display = "none";
+        }
+    });
+
+    paymentOptions.forEach((option) => {
+        option.addEventListener("change", () => {
+            if (option.value === "credit-card") {
+                creditCardFields.querySelectorAll("input, select").forEach((element) => {
+                    element.disabled = false;
+                });
+                bankTransferFields.querySelectorAll("input, select").forEach((element) => {
+                    element.disabled = true;
+                });
+            } else if (option.value === "bank-transfer") {
+                creditCardFields.querySelectorAll("input, select").forEach((element) => {
+                    element.disabled = true;
+                });
+                bankTransferFields.querySelectorAll("input, select").forEach((element) => {
+                    element.disabled = false;
+                });
+            }
+        });
+    });
+});
+
+/*const shippingTypeSelect = document.getElementById("shippingType");
+const streetInput = document.getElementById("street");
+const numberInput = document.getElementById("number");
+const cornerInput = document.getElementById("corner");
+const confirmButton = document.getElementById("confirmButton");
+
+confirmButton.addEventListener("click", confirmShipping);
+
+function confirmShipping() {
+    const selectedShippingType = shippingTypeSelect.value;
+    const selectedStreet = streetInput.value;
+    const selectedNumber = numberInput.value;
+    const selectedCorner = cornerInput.value;
+
+
+}*/
+function modificar(data) {
+    let subtotalProducto = document.getElementById('subtotalProducto');
+    let cant = document.getElementById('cantidad');
+    let cantidad = parseInt(cant.value);
+    let subtotal = parseInt(cantidad * data);
+    subtotalProducto.innerHTML = `${subtotal}`;
+}
+
+function recalcular() {
+    let cantidad = document.getElementsByClassName("cantidad");
+    let costo = document.getElementsByClassName("costo");
+    let subTotal = document.getElementsByClassName("subtotal");
+
+    for (let i = 0; i < cantidad.length; i++) {
+        let cantidadValor = parseInt(cantidad[i].value);
+        let costoValor = parseFloat(costo[i].textContent);
+        let subtotal = cantidadValor * costoValor;
+        subTotal[i].textContent = subtotal;
+    }
+}
+
+
+function ShowCartData(data) {
+    let htmlContentToAppend = "";
+    htmlContentToAppend += `<tr>   
+                    <td><img class="img-fluid" src="${data.image}" alt=""></td>
+                    <td class="align-middle">${data.name}</td>
+                    <td class="align-middle">${data.currency} ${data.unitCost}</td>
+                    <td class="align-middle"><input type="number" value="${data.count}" class="w-25 text-center" min="1" id="cantidad" onchange="modificar(${data.unitCost})"></td>
+                    <td class="align-middle"><b>${data.currency}&nbsp;</b><b id="subtotalProducto">${data.unitCost}</b></td>
+                    </tr>`;
+    cart.innerHTML = htmlContentToAppend;
+}
+
+
+
+
+
+function completarCarro() {
+    let carrito = JSON.parse(localStorage.getItem("Carrito"));
+    let htmlContentToAppend = "";
+
+    for (let producto of carrito) {
+        htmlContentToAppend += `<tr>   
+        <td><img class="img-fluid" src="${producto.image}" alt=""></td>
+        <td class="align-middle">${producto.name}</td>
+        <td class="align-middle">${producto.currency} <span class="costo">${producto.unitCost}</span></td>
+        <td class="align-middle"><input type="number" value="${producto.count}" class="w-25 text-center cantidad" min="1"  onchange="recalcular()"></td>
+        <td class="align-middle"><b>${producto.currency}&nbsp;</b><b> <span class="subtotal">${producto.unitCost}</span></b></td>
+        </tr>`;
+    }
+    cart.innerHTML += htmlContentToAppend;
+}
+
+(() => {
+    'use strict'
+    let formModal = document.getElementById("formModal");
+    let formCompra = document.getElementById("formCompra");
+    let divFormadePago = document.getElementById("pago");
+    const selectPaymentButton = document.getElementById("select-payment-button");
+  
+      formCompra.addEventListener("submit", event => {
+        divFormadePago.innerHTML = "";
+        formCompra.classList.add('was-validated');
+        if (!formModal.checkValidity()) {
+        event.preventDefault();
+        divFormadePago.classList.add('feedback-negativo');
+        divFormadePago.innerHTML = "Debe seleccionar una forma de pago";
+        selectPaymentButton.classList.add('feedback-negativo');
+        }
+        
+        
+        if (!formCompra.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        
+        
+        if(formCompra.checkValidity() && formModal.checkValidity()) {
+            event.preventDefault();
+            Swal.fire({
+                title: 'Compra realizada',
+                icon: 'success',
+                showCancelButton: false,
+            })
+            .then(() => {
+                window.location.href = "cart.html";
+            });
+        }
+      }, false)
+      formModal.addEventListener("submit", event => {
+        if (!formModal.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          formModal.classList.add('was-validated');
+    if (formModal.checkValidity()) {
+      event.preventDefault();
+      divFormadePago.classList.remove('feedback-negativo');
+      divFormadePago.innerHTML = "";
+      selectPaymentButton.classList.remove('feedback-negativo');
+    }
+}, false);
+    
+  })()
+
+/*let shippingHtml = `
+
+            <label for="shippingType">Tipo de Envío:</label>
+            <select id="shippingType" name="shippingType">
+              <option value="premium">Premium 2 a 5 días (15%)</option>
+              <option value="express">Express 5 a 8 días (7%)</option>
+              <option value="standard">Standard 12 a 15 días (5%)</option>
+            </select>
+         
+            <label for="street">Calle:</label>
+            <input type="text" id="street" name="street" required>
+         
+            <label for="number">Número:</label>
+            <input type="text" id="number" name="number" required>
+         
+            <label for="corner">Esquina:</label>
+            <input type="text" id="corner" name="corner">
+         
+            <button id="confirmButton">Confirmar Envío</button>`;
+
+
+cart.innerHTML += shippingHtml;*/
---
+++ js/product-info.js
@@ -0,0 +1,196 @@
+let ProdID = localStorage.getItem("ProdID");
+let divProducto = document.getElementById("infoProducto");
+PRODUCTS_COMMENTS = `https://japceibal.github.io/emercado-api/products_comments/${ProdID}.json`;
+let URL_PRODUCTO = `https://japceibal.github.io/emercado-api/products/${ProdID}.json`;
+let filledStar = `<i class="fas fa-star" style="color: rgb(218, 165, 32)"></i>`
+let emptyStar = `<i class="far fa-star" style="color: #000000;"></i>`;
+let divRelacionados = document.getElementById("relacionados");
+let botonComprar = document.getElementById("addCart");
+
+function hora() {
+
+    const fechaHoraActual = new Date();
+
+    const año = fechaHoraActual.getFullYear();
+    const mes = String(fechaHoraActual.getMonth() + 1).padStart(2, '0');
+    const dia = String(fechaHoraActual.getDate()).padStart(2, '0');
+    const horas = String(fechaHoraActual.getHours()).padStart(2, '0');
+    const minutos = String(fechaHoraActual.getMinutes()).padStart(2, '0');
+    const segundos = String(fechaHoraActual.getSeconds()).padStart(2, '0');
+
+    return fechaHoraFormateada = `${año}-${mes}-${dia} ${horas}:${minutos}:${segundos}`;
+}
+function addData(info) {
+    let htmlContentToAppend = "";
+    let starsHtml = "";
+    for (let i = 1; i <= 5; i++) {
+        if (info.score >= i) {
+            starsHtml += filledStar;
+        } else {
+            starsHtml += emptyStar;
+        }
+    }
+    htmlContentToAppend += `
+    <div class="list-group-item list-group-item-action cursor-active">
+    <div class="row">
+        <div class="col">
+            <div class=" w-100 d-flex justify-content-between">
+            <h4 class="mb-1"> ${info.user}</h4>
+            <small class="text-muted">${info.dateTime}</small>
+            </div>
+            <div class="stars">${starsHtml}</div>
+            </div>
+            <br>
+            <p class="mb-1">${info.description}</p>
+            </div>
+            </div>
+ </div>`
+    contenedor.innerHTML += htmlContentToAppend;
+}
+function showComments(Array) {
+    for (const comment of Array) {
+        addData(comment)
+    }
+}
+
+function setProdID(id) {
+    localStorage.setItem("ProdID", id);
+    window.location = "product-info.html"
+}
+
+let carrito = [];
+
+
+function mostrarRelacionados(Array) {
+    let htmlContentToAppend = "";
+
+    for (const item of Array) {
+        htmlContentToAppend += `
+        <div class="divRel cursor-active" onclick="setProdID(${item.id})">
+            <img src="${item.image}" ><br>
+            <p>${item.name}</p>
+        </div>
+     
+        `}
+
+    divRelacionados.innerHTML = htmlContentToAppend;
+}
+
+let btnSend = document.getElementById("btnSend");
+btnSend.addEventListener("click", function () {
+    let comment = document.getElementById("comment")
+    let score = document.getElementById("rating")
+    let user = JSON.parse(sessionStorage.getItem("sesion"));
+    let now = hora();
+    let info = { user: user.usuario, score: score.value, dateTime: now, description: comment.value }
+    addData(info)
+    comment.value = ""
+    score.value = 1
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+    fetch(URL_PRODUCTO)
+        .then(response => response.json())
+        .then(data => {
+            let htmlContentToAppend = "";
+
+            htmlContentToAppend += `
+          <div class="presentation">
+            <div class="text">
+                <h2>${data.name}</h2>
+                
+                <hr>
+                <h4>Precio</h4>
+                <p>${data.currency} ${data.cost}</p>
+                <br>
+                <h4>Descripción</h4>
+                <p>${data.description}</p>
+                <br>
+                <h4>Categoría</h4>
+                <p>${data.category}</p>
+                <br>
+                <h4>Cantidad de vendidos</h4>
+                <p>${data.soldCount}</p>
+            </div>
+            <br>
+            <div class="imagenes">
+                <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="${data.images[0]}" class="d-block w-100">
+                        </div>`;
+
+            for (let i = 1; i < data.images.length; i++) {
+                htmlContentToAppend +=
+                    `<div class="carousel-item">
+                        <img src="${data.images[i]}" class="d-block w-100">
+                        </div>`;
+            }
+
+            htmlContentToAppend +=
+                `</div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+            </button>
+            </div>
+            </div>
+          </div>`;
+
+            divProducto.innerHTML = htmlContentToAppend;
+            mostrarRelacionados(data.relatedProducts);
+
+
+        })
+        .catch(error => {
+            console.error("Error al cargar los datos:", error);
+        });
+    getJSONData(PRODUCTS_COMMENTS)
+        .then(result => {
+            if (result.status === "ok") {
+                arreglo = result.data;
+                showComments(arreglo);
+            } else {
+                console.error(result.data);
+            }
+        });
+})
+
+botonComprar.addEventListener("click", () => {
+    fetch(URL_PRODUCTO)
+        .then(response => response.json())
+        .then(data => {
+            let carrito = localStorage.getItem("Carrito");
+
+            // Verificar si carrito es null o no existe en localStorage
+            if (carrito === null) {
+                carrito = [];
+            } else {
+                carrito = JSON.parse(carrito); // Convierte la cadena JSON almacenada en un array
+            }
+
+            let producto = { id: data.id, name: data.name, count: 1, unitCost: data.cost, currency: data.currency, image: data.images[0] };
+            carrito.push(producto);
+
+            // Almacenar el carrito actualizado en localStorage
+            localStorage.setItem("Carrito", JSON.stringify(carrito));
+            Swal.fire({
+                title: 'Exito',
+                text: "Producto agregado al carrito",
+                icon: 'success',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Ir al carrito',
+                cancelButtonText: 'Continuar comprando'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    window.location.href = "cart.html";
+                }
+            })
+        })
+})
